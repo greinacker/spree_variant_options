5d9df56439f982b59ce1955a280b470b200a663b
Greg Reinacker <gregr@rassoc.com>
2012-02-26 13:44:32 -0700

    removed images

--- app/models/option_value_decorator.rb
+++ app/models/option_value_decorator.rb
@@ -2,12 +2,12 @@ Spree::OptionValue.class_eval do
 
   default_scope order(:position)
     
-  has_attached_file :image, 
-    :styles => { :small => '40x30#', :large => '140x110#' },
-    :default_style => :small
-  
-  def has_image?
-    image_file_name && !image_file_name.empty?
-  end
-  
+  # has_attached_file :image, 
+  #   :styles => { :small => '40x30#', :large => '140x110#' },
+  #   :default_style => :small
+  # 
+  # def has_image?
+  #   image_file_name && !image_file_name.empty?
+  # end
+  # 
 end
