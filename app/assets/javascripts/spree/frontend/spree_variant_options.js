3cb25ae94bc413c7b62bcd27afd89903d22336c1
Greg Reinacker <gregr@rassoc.com>
2015-04-06 17:22:35 -0600

    update js paths

--- app/assets/javascripts/spree/frontend/spree_variant_options.js
+++ app/assets/javascripts/spree/frontend/spree_variant_options.js
@@ -1,4 +1,2 @@
-//= require store/spree_frontend
-
-//= require store/product_variant_options
-//= require store/variant_options
+//= require spree/frontend/product_variant_options
+//= require spree/frontend/variant_options
